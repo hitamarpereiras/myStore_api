7f6e222a7ca7160a56d598fb7004884eb1f79720
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-23 09:28:45 -0300

    app accounts atualizado

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from services.idgenerator_svc import generate_id
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.CharField(
+        primary_key=True,
+        default=generate_id,
+        max_length=10,
+        editable=False
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    telephone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name='Telefone'
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username'
+    ]
+
+    def __str__(self):
+        return self.email
