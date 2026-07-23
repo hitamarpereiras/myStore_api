69f3aaebc668c44f42a06ef8124dbda8f8a16cea
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-23 09:19:00 -0300

    inicio do projeto app/accounts criado

---
+++ config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class EnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+
+    # Cofiguracoes gerais
+    APP_NAME: str = "my store"
+    DEBUG: bool = False
+    DATABASE_URL: str
+
+    # Segredos 
+    """SECRET_KEY: str
+    STRIPE_API_KEY: str"""
+
+# Instância global para uso em toda a aplicação
+thi_settings = EnvSettings()
