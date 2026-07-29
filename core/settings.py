4f4d617ae14b714530f6a2408cb61fc329e8a090
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-29 13:47:52 -0300

    atualizacao em rest_f em settings

--- core/settings.py
+++ core/settings.py
@@ -145,12 +145,20 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 
 }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=15),  # tempo do token de acesso
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),     # tempo do refresh
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),     # tempo do refresh
     'ROTATE_REFRESH_TOKENS': True, # Gera um novo  refresh token a cada refresh 
     'BLACKLIST_AFTER_ROTATION': True, # Invalida o refresh token antigo após a rotação
 }
