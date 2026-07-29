7b0ab67951a4a03b8ab8799dc9dcb06d427786ce
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-29 19:45:48 -0300

    correcao na funcao de gerar id_store

--- services/idgenerator_svc.py
+++ services/idgenerator_svc.py
@@ -5,7 +5,7 @@ def generate_id():
     return uuid.uuid4().hex[:6].upper()
 
 def generate_idStore():
-    return uuid.uuid4().hex[:10].upper()
+    return uuid.uuid4().hex[:6].upper()
 
 def generate_idProduct():
     return uuid.uuid4().hex[:14].lower()
