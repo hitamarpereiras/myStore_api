3a2e0e742ee38189ddb638ad7a0415b471fe110a
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-29 13:22:40 -0300

    authentication aplicada em settings

--- core/settings.py
+++ core/settings.py
@@ -34,10 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework',
     'django_filters',
 
+    'apps.authentication',
     'apps.accounts',
     'apps.stores',
 ]
