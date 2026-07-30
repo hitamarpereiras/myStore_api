7ca6cebb35a6fb5de765aadf40b0d15c119bf6c1
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-30 09:36:57 -0300

    atualizacao em core/urls

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('api/v1/', include('apps.authentication.urls')),
     path('api/v1/', include('apps.accounts.urls')),
     path('api/v1/', include('apps.stores.urls')),
+    path('adm/', admin.site.urls),
 ]
