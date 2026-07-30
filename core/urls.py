be13eb861c33e807e5111de260329b1e7b581336
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-30 10:08:47 -0300

    page home add

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 
 urlpatterns = [
+    path('', home),
     path('api/v1/', include('apps.authentication.urls')),
     path('api/v1/', include('apps.accounts.urls')),
     path('api/v1/', include('apps.stores.urls')),
