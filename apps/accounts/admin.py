c70461e5c59fdfe0c4bfec994c0374d32dfe9a1c
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-08-03 22:25:41 -0300

    3 teste de correcao apps/accounts

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,22 +1,4 @@
 from django.contrib import admin
 from apps.accounts.models import User
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'email',
-        'first_name',
-        'last_name',
-        'is_active',
-        'telephone'
-    )
-
-    list_filter = (
-        'is_active',
-    )
-    search_fields = (
-        'email',
-        'first_name',
-        'telephone'
-    )
+admin.site.register(User)
