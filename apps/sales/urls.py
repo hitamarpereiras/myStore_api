d3ed6bd2c1a9402c437a50f76231a6c625baadd1
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-08-08 10:59:47 -0300

    feat(stores) migrations criadas

--- apps/sales/urls.py
+++ apps/sales/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from sales.views import SaleViewSet
+from apps.sales.views import SaleViewSet
 
 
 router = DefaultRouter()
-router.register(r'sales', SaleViewSet)
+router.register(r'sales', SaleViewSet, basename='sales')
 
 urlpatterns = [
     path('', include(router.urls)),
