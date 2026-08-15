71a0d0e8836df203dc42efd533f14b623cf33eba
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-08-15 10:40:32 -0300

    teste 4 de correcao sigskill out memory

--- apps/orders/apps.py
+++ apps/orders/apps.py
@@ -5,7 +5,5 @@ class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.orders"
 
-    """
     def ready(self):
-        import apps.orders.signals
-    """
+        import orders.signals
--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+print(">>> WSGI 1")
 
 from django.core.wsgi import get_wsgi_application
+print(">>> WSGI 2")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+print(">>> WSGI 3")
 
 application = get_wsgi_application()
+print(">>> WSGI 4")
