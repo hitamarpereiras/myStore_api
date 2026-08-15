78b0e3be8da40ecc96e72f5fcdce37333f9f1890
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-08-15 11:04:15 -0300

    teste 7 de correcao sigskill out memory

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,19 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-print(">>> WSGI 1")
+
 
 from django.core.wsgi import get_wsgi_application
 
-print(">>> WSGI 2")
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-print(">>> WSGI 3")
 
-try:
-    application = get_wsgi_application()
-    print(">>> WSGI 4 - DJANGO INICIALIZADO")
-except Exception as e:
-    print(">>> ERRO AO INICIALIZAR DJANGO:", repr(e))
-    raise
+application = get_wsgi_application()
